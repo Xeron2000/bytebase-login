ad664b737ea0717f004375b0c18c4bd442c04553
Xeron <cw48565@gmail.com>
2025-07-31 18:20:49 +0800

    fix: update repoName in next.config.ts to use root path for static export

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,14 @@
 import type { NextConfig } from "next";
 
 const isStaticExport = process.env.STATIC_EXPORT === 'true';
-const repoName = '/bytebase-login';
 
 const nextConfig: NextConfig = {
   // Conditionally set output, basePath, and assetPrefix for static exports
   output: isStaticExport ? "export" : undefined,
-  basePath: isStaticExport ? repoName : undefined,
-  assetPrefix: isStaticExport ? repoName : undefined,
+  basePath: isStaticExport ? "/bytebase-login" : undefined,
 
   images: {
+    unoptimized: isStaticExport ? true : false,
     remotePatterns: [
       {
         protocol: 'https',

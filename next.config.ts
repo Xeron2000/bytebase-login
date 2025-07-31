c5ec1ed3adc79bdd4e53171d1059cb0bfcac9ec1
Xeron <cw48565@gmail.com>
2025-07-31 17:05:58 +0800

    fix: enhance static export configuration by adding basePath and assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const isStaticExport = process.env.STATIC_EXPORT === 'true';
+const repoName = '/bytebase-login';
 
 const nextConfig: NextConfig = {
-  // Conditionally set output based on the environment variable
+  // Conditionally set output, basePath, and assetPrefix for static exports
   output: isStaticExport ? "export" : undefined,
+  basePath: isStaticExport ? repoName : undefined,
+  assetPrefix: isStaticExport ? repoName : undefined,
 
   images: {
     remotePatterns: [
